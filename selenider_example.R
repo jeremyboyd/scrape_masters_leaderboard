ed6ee24e63f0d94cf1a90f12ab782c756829f867
jeremyboyd <7953134+jeremyboyd@users.noreply.github.com>
2024-04-11 20:40:39 -0600

    Clean up R project example.

--- selenider_example.R
+++ selenider_example.R
@@ -1,26 +1,24 @@
-# Check out slenider package for scraping. Is able to open a headed browser session, but doesn't seem to be good at finding page elements
+# Author: Jeremy Boyd (jeremyboyd@pm.me)
+# Description: Explore the use of selenider package to scrape webpages.
 
-
+# Packages
 library(tidyverse)
 library(selenider)
 library(rvest)
 
-
-# Start session. Headless by default
-# session <- selenider_session(session = "chromote")
-
-# Can do a headed session like this, which means that chrome will open and show page
-# Might need to open this on a different port because already using port in other session. Can't set port in chromote options...
+# Open a headed session. With a headed session you can watch in Chrome as the
+# code below operates the browser.
 session <- selenider_session(
     session = "chromote",
     options = chromote_options(headless = FALSE))
 
+# Won't open this page
+open_url("https://www.masters.com/en_US/scores/index.html")
 
-
-# Got to r project
+# But will open this one
 open_url("https://www.r-project.org/")
 
-# Finds most recent mastadon post on page and follows URL
+# Finds most recent mastodon post on page and follows URL
 s(".mt-timeline") |>
     find_element("article") |>
     elem_attr("data-location") |>
@@ -32,29 +30,5 @@ s(".columns-area") |>
     read_html() |>
     html_text2()
 
-
-
-
-
-
-# Select all links 
-ss("a")
-
-
-ss("a") |>
-    find_element("li:nth-child(4) a")
-
-
-
-menu_items <- s("#rStudioHeader") |>
-    find_element("#menu") |>
-    find_elements(".menuItem")
-
-
-# Go to masters leaderboard
-open_url("https://www.masters.com/en_US/scores/index.html")
-
-# Won't select heading level 1. Makes me think that selenider can't do this live.
-ss("h1")
-
-
+# Go back to R project
+open_url("https://www.r-project.org/")
